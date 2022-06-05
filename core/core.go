45d79433eb4951d8f8627a6bd864d0191dc05978
fmerg <fmerg@protonmail.com>
2022-06-05 10:34:36 +0300

    Add first version of Paillier enc/dec

--- core/core.go
+++ core/core.go
@@ -12,6 +12,66 @@ import (
 )
 
 
+func EulerPhi(p *big.Int, q *big.Int) *big.Int {
+  one := big.NewInt(1)
+  pMinusOne := new(big.Int).Sub(p, one)
+  qMinusOne := new(big.Int).Sub(q, one)
+  return new(big.Int).Mul(pMinusOne, qMinusOne)
+}
+
+
+func randomness(n *big.Int) *big.Int {
+  r, err := rand.Int(rand.Reader, n)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  return r
+}
+
+
+func Encrypt(N *big.Int, m *big.Int) *big.Int {
+  one := big.NewInt(1)
+  two := big.NewInt(2)
+
+  onePlusN := new(big.Int).Add(N, one)              // 1 + N
+  M := new(big.Int).Exp(N, two, nil)                // N ^ 2
+
+  onePlusNToM := new(big.Int).Exp(onePlusN, m, nil) // (1 + N) ^ m
+
+  r := randomness(N)
+  rToN := new(big.Int).Exp(r, N, nil)         // r ^ N
+
+  aux := new(big.Int).Mul(onePlusNToM, rToN)  // (1 + N) ^ m * r ^ N
+  c := new(big.Int).Mod(aux, M)               // (1 + N) ^ m * r ^ N (mod N ^ 2)
+  return c
+}
+
+
+func Decrypt(p *big.Int, q *big.Int, c *big.Int) *big.Int {
+  one := big.NewInt(1)
+  two := big.NewInt(2)
+
+  N := new(big.Int).Mul(p, q)                   // N
+  M := new(big.Int).Exp(N, two, nil)            // N ^ 2
+
+  totient := EulerPhi(p, q)                     // phi(N)
+
+  c_hat := new(big.Int).Exp(c, totient, M)      // c ^ phi(N) (mod N ^ 2)
+
+  tmp := new(big.Int).Sub(c_hat, one)
+  m_hat := new(big.Int).Div(tmp, N)     // (c ^ phi(N) (mod N ^ 2) - 1)/N
+
+  totientInverse := new(big.Int).ModInverse(totient, N) // phi(N) ^ -1 (mod N ^ 2)
+
+  tmp2 := new(big.Int).Mul(m_hat, totientInverse)
+  d := new(big.Int).Mod(tmp2, N)
+
+  return d
+}
+
+
 func Setup() elliptic.Curve {
 
   return elliptic.P256()
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
   "core"
   "fmt"
+  "math/big"
 )
 
 
@@ -25,6 +26,21 @@ func demoFlow() {
   signature := core.SignASN1(message, key)
   verified = core.VerifySignatureASN1(message, &public, signature)
   fmt.Println(verified)
+
+  // Paillier enc/dec
+  p := big.NewInt(11)
+  q := big.NewInt(17)
+  N := new(big.Int).Mul(p, q)
+  fmt.Println("N:", N)
+
+  m := big.NewInt(175)
+  fmt.Println("m:", m)
+
+  c := core.Encrypt(N, m)
+  fmt.Println("c:", c)
+
+  d := core.Decrypt(p, q, c)
+  fmt.Println("d:", d)
 }
 
 
