f2ce0d92f78a21183f084aebfd953fcc4519a6e2
fmerg <fmerg@protonmail.com>
2022-06-06 16:38:09 +0300

    Create paillier/util module

--- paillier/paillier.go
+++ paillier/paillier.go
@@ -1,9 +1,7 @@
 package paillier
 
 import (
-  "crypto/rand"
   "math/big"
-  "log"
   "fmt"
 )
 
@@ -111,42 +109,4 @@ func (key *Key) Decrypt(cipher *big.Int) *big.Int {
   result := new(big.Int).Mul(m_hat, key.totientInv) // ((c^ -1) / N) * (phi(N) ^ -1 (mod N))
   result = result.Mod(result, key.N)  // ((c^ -1) / N) * (phi(N) ^ -1 (mod N))  (mod N)
   return result
-}
-
-
-func randInt(max *big.Int) *big.Int {
-  r, err := rand.Int(rand.Reader, max)
-
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  return r
-}
-
-
-// Generate prime numbers p, q = (p - 1)/2 with bitlength(p) >= bitLen
-func GenerateSafePrimes(bitLength int) (*big.Int, *big.Int) {
-  p := new(big.Int)
-
-  count := 0
-  for {
-    fmt.Println(count)
-    count ++
-    q, err := rand.Prime(rand.Reader, bitLength - 1)
-
-    if err != nil {
-      log.Fatal(err)
-    }
-
-    // p = 2 * q + 1 = (q << 1) ^ 1
-    p.Lsh(q, 1)
-    p.SetBit(p, 0, 1)
-
-    // Miller-Rabin primality test for p failing with probability at most equal
-    // to (1/4) ^ 25
-    if p.ProbablyPrime(25) {
-      return p, q
-    }
-  }
 }
---
+++ paillier/util.go
@@ -0,0 +1,47 @@
+package paillier
+
+
+import (
+  "crypto/rand"
+  "math/big"
+  "log"
+  "fmt"
+)
+
+
+func randInt(max *big.Int) *big.Int {
+  r, err := rand.Int(rand.Reader, max)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  return r
+}
+
+
+// Generate prime numbers p, q = (p - 1)/2 with bitlength(p) >= bitLen
+func GenerateSafePrimes(bitLength int) (*big.Int, *big.Int) {
+  p := new(big.Int)
+
+  count := 0
+  for {
+    fmt.Println(count)
+    count ++
+    q, err := rand.Prime(rand.Reader, bitLength - 1)
+
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    // p = 2 * q + 1 = (q << 1) ^ 1
+    p.Lsh(q, 1)
+    p.SetBit(p, 0, 1)
+
+    // Miller-Rabin primality test for p failing with probability at most equal
+    // to (1/4) ^ 25
+    if p.ProbablyPrime(25) {
+      return p, q
+    }
+  }
+}
